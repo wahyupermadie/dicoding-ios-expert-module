f54caf6bf614c354764f133365b7045d9a67753e
Wahyu Permadi <wahyu.permadi1725@gmail.com>
2021-02-14 14:50:11 +0800

    refactor(profile-view): change to public

--- Profile/Presentation/ProfileView.swift
+++ Profile/Presentation/ProfileView.swift
@@ -7,14 +7,14 @@
 
 import SwiftUI
 
-struct ProfileView: View {
-    var body: some View {
+public struct ProfileView: View {
+    public var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-struct ProfileView_Previews: PreviewProvider {
-    static var previews: some View {
+public struct ProfileView_Previews: PreviewProvider {
+    public static var previews: some View {
         ProfileView()
     }
 }
